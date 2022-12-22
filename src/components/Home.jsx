17163476f85c06526ec682da26131ed0f3d41b67
Yuvraj <kingyuvi88@gmail.com>
2022-12-23 04:58:05 +0530

    Added calculate fare function.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,61 @@
 import Axios from 'axios';
+import { useState } from 'react';
 function Home(){
-    const from = document.getElementById('from');
-    const to = document.getElementById('to');
+    const [from, setFrom] = useState("Janak Puri");
+     
+    const [to, setTo] = useState("Tagore Garden");
+    
     const key = 'JuNkCXmB4VXmxBy3rsUkrspPpnYYj';
-    const url = `https://api.distancematrix.ai/maps/api/distancematrix/json?origins=${from.value}&destinations=${to.value}&departure_time=now&key=JuNkCXmB4VXmxBy3rsUkrspPpnYYj`; 
+    const url = `https://api.distancematrix.ai/maps/api/distancematrix/json?origins=${from}&destinations=${to}&departure_time=now&key=JuNkCXmB4VXmxBy3rsUkrspPpnYYj`; 
+
+    function changeFrom(event){
+        const newFrom = event.target.value; 
+        setFrom(newFrom);
+    }
+    function changeTo(event){
+        const newTo = event.target.value; 
+
+        setTo(newTo);
+    }
+
+    let distance; 
 
     function getDistance(){
         Axios.get(url).then((response)=>{
-            console.log(response.data.rows);
-            console.log(response.data.destination_addresses);
+            let d = response?.data?.rows[0]?.elements[0]?.distance.text; 
+           d = d.split(" ");
+            // let match =d.match(/(\d+)/);
+            // distance = match[0];
+            distance = d[0];
+            // console.log(typeof distance);
+            // console.log(distance);
+            // const 
+            // console.log(match);
+            // console.log();
         })
+        calculateFare();
+
+// console.log(to);
+    }
+    const fixed = 25; 
+    const fpk = 8
+    function calculateFare(){
+        console.log("Calculating fair: ");
+        if(+distance<=2){
+            console.log("Fare=",fixed); 
+        }
+        else if(+distance>2){
+            const fare = +distance*fpk; 
+            console.log("Fare: ",fare); 
+            console.log(fare);
+        }
     }
     
     return(
         <>
-        <div>
-            <input type="text" placeholder="From" id='from'></input><br/>
-            <input type="text" placeholder="To" id='to'></input><br/>
+        <div> Jai Bajrang Bali
+            <input type="text" placeholder="From" value={from} onChange={changeFrom} id='from'></input><br/>
+            <input type="text" value={to} placeholder="To" id='to' onChange={changeTo}></input><br/>
             <button onClick={getDistance}>Submit</button>
             
         
